14d140a11af20ab39eb861d4331dd2becfb72315
lisa <104255899+lemongad@users.noreply.github.com>
2023-06-09 20:22:13 +0800

    latest

---
+++ files/entrypoint.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+
+# 设置各变量
+WSPATH=${WSPATH:-'argo'}
+UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+WEB_USERNAME=${WEB_USERNAME:-'admin'}
+WEB_PASSWORD=${WEB_PASSWORD:-'password'}
+
+
+generate_argo() {
+  cat > /tmp/argo.sh << ABC
+#!/usr/bin/env bash
+
+argo_type() {
+  if [[ -n "\${ARGO_AUTH}" && -n "\${ARGO_DOMAIN}" ]]; then
+    [[ \$ARGO_AUTH =~ TunnelSecret ]] && echo \$ARGO_AUTH > /tmp/tunnel.json && cat > /tmp/tunnel.yml << EOF
+tunnel: \$(cut -d\" -f12 <<< \$ARGO_AUTH)
+credentials-file: /tmp/tunnel.json
+protocol: http2
+
+ingress:
+  - hostname: \$ARGO_DOMAIN
+    service: http://localhost:8081
+  - hostname: \$WEB_DOMAIN
+    service: http://localhost:3000
+EOF
+
+    [ -n "\${SSH_DOMAIN}" ] && cat >> /tmp/tunnel.yml << EOF
+  - hostname: \$SSH_DOMAIN
+    service: http://localhost:222
+EOF
+
+    cat >> /tmp/tunnel.yml << EOF
+    originRequest:
+      noTLSVerify: true
+  - service: http_status:404
+EOF
+
+  else
+    ARGO_DOMAIN=\$(cat /tmp/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s%.*https://%%g" | tail -n 1)
+  fi
+}
+
+export_list() {
+  VMESS="{ \"v\": \"2\", \"ps\": \"Argo-Vmess\", \"add\": \"icook.hk\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\${ARGO_DOMAIN}\", \"path\": \"/${WSPATH}-vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"\${ARGO_DOMAIN}\", \"alpn\": \"\" }"
+
+  cat > /tmp/list << EOF
+*******************************************
+V2-rayN:
+----------------------------
+vless://${UUID}%icook.hk:443?encryption=none&security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=%2F${WSPATH}-vless?ed=2048#Argo-Vless
+----------------------------
+vmess://\$(echo \$VMESS | base64 -w 0)
+----------------------------
+trojan://${UUID}%icook.hk:443?security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=%2F${WSPATH}-trojan?ed=2048#Argo-Trojan
+----------------------------
+ss://$(echo "chacha20-ietf-poly1305:${UUID}%icook.hk:443" | base64 -w 0)%icook.hk:443#Argo-Shadowsocks
+由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: \${ARGO_DOMAIN} ，路径: /${WSPATH}-shadowsocks?ed=2048 ， 传输层安全: tls ， sni: \${ARGO_DOMAIN}
+*******************************************
+小火箭:
+----------------------------
+vless://${UUID}%icook.hk:443?encryption=none&security=tls&type=ws&host=\${ARGO_DOMAIN}&path=/${WSPATH}-vless?ed=2048&sni=\${ARGO_DOMAIN}#Argo-Vless
+----------------------------
+vmess://$(echo "none:${UUID}%icook.hk:443" | base64 -w 0)?remarks=Argo-Vmess&obfsParam=\${ARGO_DOMAIN}&path=/${WSPATH}-vmess?ed=2048&obfs=websocket&tls=1&peer=\${ARGO_DOMAIN}&alterId=0
+----------------------------
+trojan://${UUID}%icook.hk:443?peer=\${ARGO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARGO_DOMAIN};obfs-uri=/${WSPATH}-trojan?ed=2048#Argo-Trojan
+----------------------------
+ss://$(echo "chacha20-ietf-poly1305:${UUID}%icook.hk:443" | base64 -w 0)?obfs=wss&obfsParam=\${ARGO_DOMAIN}&path=/${WSPATH}-shadowsocks?ed=2048#Argo-Shadowsocks
+*******************************************
+Clash:
+----------------------------
+- {name: Argo-Vless, type: vless, server: icook.hk, port: 443, uuid: ${UUID}, tls: true, servername: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: /${WSPATH}-vless?ed=2048, headers: { Host: \${ARGO_DOMAIN}}}, udp: true}
+----------------------------
+- {name: Argo-Vmess, type: vmess, server: icook.hk, port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /${WSPATH}-vmess?ed=2048, headers: {Host: \${ARGO_DOMAIN}}}, udp: true}
+----------------------------
+- {name: Argo-Trojan, type: trojan, server: icook.hk, port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: /${WSPATH}-trojan?ed=2048, headers: { Host: \${ARGO_DOMAIN} } } }
+----------------------------
+- {name: Argo-Shadowsocks, type: ss, server: icook.hk, port: 443, cipher: chacha20-ietf-poly1305, password: ${UUID}, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: \${ARGO_DOMAIN}, path: /${WSPATH}-shadowsocks?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
+*******************************************
+EOF
+  cat /tmp/list
+}
+
+argo_type
+export_list
+ABC
+}
+
+generate_pm2_file() {
+  if [[ -n "${ARGO_AUTH}" && -n "${ARGO_DOMAIN}" ]]; then
+    [[ $ARGO_AUTH =~ TunnelSecret ]] && ARGO_ARGS="tunnel --edge-ip-version auto --config /tmp/tunnel.yml --url http://localhost:8081 run"
+    [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]] && ARGO_ARGS="tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH}"
+  else
+    ARGO_ARGS="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile /tmp/argo.log --loglevel info --url http://localhost:8081"
+  fi
+
+  TLS=${NEZHA_TLS:+'--tls'}
+
+ 
+
+  cat > /tmp/ecosystem.config.js << EOF
+module.exports = {
+  "apps":[
+      {
+          "name":"web",
+          "script":"/app/web"
+      },
+      {
+          "name":"a",
+          "script":"/app/c.js_amd64",
+          "args":"${ARGO_ARGS}"
+	  },
+	  {   "name":"nm",
+          "script":"/app/nm",
+          "args":"-s ${NEZHA_S}:${NEZHA_P} -p ${NEZHA_K} "
+EOF
+
+  [ -n "${SSH_DOMAIN}" ] && cat >> /tmp/ecosystem.config.js << EOF
+      },
+      {
+          "name":"ttyd",
+          "script":"/app/ttyd",
+          "args":"-c ${WEB_USERNAME}:${WEB_PASSWORD} -p 222 bash"
+EOF
+
+  cat >> /tmp/ecosystem.config.js << EOF
+      }
+  ]
+}
+EOF
+}
+
+generate_config
+generate_argo
+generate_pm2_file
+
+[ -e /tmp/argo.sh ] && bash /tmp/argo.sh
+[ -e /tmp/ecosystem.config.js ] && pm2 start /tmp/ecosystem.config.js
